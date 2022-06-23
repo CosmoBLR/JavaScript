27d030d0ae82d781e4c6b1b8f14702b9722fc784
CosmoBLR <ekozlov89@gmail.com>
2022-06-23 22:03:25 +0300

    Async part 2

--- 07_async.js
+++ 07_async.js
@@ -28,3 +28,29 @@ const delay = (wait = 1000) => {
     return promise
 }
 
+delay(2500)
+// .then .catch .finally методы
+    .then(() => {
+        console.log('After 2 seconds')
+    })
+    .catch(err => console.error('Error:', err))
+    .finally(() => console.log('Finally'))
+
+const getData = () => new Promise(resolve => resolve([
+    1, 1, 2, 3, 5, 8, 13
+]))
+getData().then(data => console.log(data))
+
+async function asyncExample() {
+    try {
+    await delay(3000)
+    const data = await getData()
+    console.log('Data', data)
+    
+    } catch (e) {
+    console.log(e)
+    } finally {
+        console.log('Finally')
+    }
+}
+asyncExample()
